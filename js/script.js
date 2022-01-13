348abce3a264d36f99fb3973d3e7ecdef5f190da
Glitch (detongress-rps) <none>
2022-01-13 00:30:22 +0000

    🌬️✂️ Updated with Glitch

--- js/script.js
+++ js/script.js
@@ -1,1 +1,21 @@
-
+$(".play").click(function(){
+  let userInput=$(".input").val()
+  $(".userChoice").text(userInput)
+  let randomNumber=Math.ceil(Math.random()*3)
+  if(randomNumber===1){
+    $(".computerChoice").text("rock")
+  } else if(randomNumber===2){
+    $(".computerChoice").text("paper")
+  } else if(randomNumber===3){
+    $(".computerChoice").text("scissors")
+  }
+  
+  //If the user chooses rock and the computer chooses scissors, the user wins
+  if(userInput==="rock" && randomNumber===3 ){
+    $(".result").text("User Wins")
+  } else if(userInput==="rock" && randomNumber===2){
+    $(".result").text("Computer Wins")
+  } else if(userInput==="paper" && randomNumber===3){
+    $(".result").text("Computer Wins")
+  } 
+})
